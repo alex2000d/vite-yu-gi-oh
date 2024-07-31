1e5c945be9ea27813b4a9bdee1dd426613cd71e3
alex2000d <demarcoalessandro777@gmail.com>
2024-07-31 17:14:47 +0200

    [UPD] added the header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <script>
+import { store } from './store.js'
+import axios from 'axios';
+import Appheader from './components/Appheader.vue';
 export default {
-  
+  components:{
+   Appheader,
+  },
+  created(){
+
+  },
+  methods:{
+       getcardlist(){
+        axios.get(store.apiurl).then((result)=>{
+          // store.cardlist =
+        })
+       }
+  },
+
+  data(){
+      return{
+        store
+      }
+  }
 }
 </script>
 <template lang="">
   <div>
-    
+    <Appheader />
   </div>
 </template>
 <style lang="scss">
--- src/components/Appheader.vue
+++ src/components/Appheader.vue
@@ -3,11 +3,18 @@ export default {
     
 }
 </script>
-<template lang="">
-    <div>
-        
-    </div>
+<template>
+    <header>
+        <div class="logo d-flex">
+            <div class="image align-middle">
+                <img src="../assets/yugioh-logo-png.png">
+            </div>
+            <h2 class="py-3">YU-GI-OH API</h2>
+        </div>
+    </header>
 </template>
-<style lang="">
-    
+<style lang="scss">
+    .logo img{
+        width: 200px;
+    }
 </style>
